06bcc963557b359e7636ee5ecb8ac73c5cdd7f87
home <home@homes-MacBook-Pro.local>
2021-03-23 14:09:55 +0700

    add code to github

---
+++ Calculator/build/precache-manifest.8c605a413e0af4914852c9ef4d1963bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240c66dcf5fd5a733a59e18e95753312",
+    "url": "/www/calculator/index.html"
+  },
+  {
+    "revision": "b4caa24ab9278607995f",
+    "url": "/www/calculator/static/css/main.86e3fc58.chunk.css"
+  },
+  {
+    "revision": "4efdd5f72145c1cca6b2",
+    "url": "/www/calculator/static/js/2.50244f75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www/calculator/static/js/2.50244f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4caa24ab9278607995f",
+    "url": "/www/calculator/static/js/main.50ea9127.chunk.js"
+  },
+  {
+    "revision": "d0cc9f3e449e59f12a28",
+    "url": "/www/calculator/static/js/runtime-main.86283cf4.js"
+  }
+]);
